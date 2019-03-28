05ef4d4c2bfa043609658187d56b76b950cfb48f
Tristan Figour <noblaya@gmail.com>
2019-03-28 12:53:15 +0100

    Product Class : - Add Getter to : name, description, price - Add Setter to : price
    - Add SOUT to look method

--- src/com/tfi/OC_JavaObj/Product.java
+++ src/com/tfi/OC_JavaObj/Product.java
@@ -1,15 +1,15 @@
 package com.tfi.OC_JavaObj;
 
 public class Product {
-    String name;
-    String description;
-    double price;
+    private String name;
+    private String description;
+    private double price;
 
     /**
      * Display a full description of the product
      */
     public void look() {
-
+        System.out.println(String.format(name + " : " + price + "%n" + description));
     }
 
     /**
@@ -20,4 +20,18 @@ public class Product {
     public void buy(Bill bill, Integer quantity){
 
     }
+
+    public String getName() {
+        return name;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public double getPrice() {
+        return price;
+    }
+    public void setPrice(double price){
+        this.price = price;
+    }
+
 }
