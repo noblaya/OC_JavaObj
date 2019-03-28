1734c77b8e3cd6dee0357dae002cc6bed3418255
Tristan Figour <noblaya@gmail.com>
2019-03-28 13:11:46 +0100

    Edit Bill class - Add addProduct (with javadoc), getCustomet, getProduct methods.

--- src/com/tfi/OC_JavaObj/Bill.java
+++ src/com/tfi/OC_JavaObj/Bill.java
@@ -1,4 +1,25 @@
 package com.tfi.OC_JavaObj;
 
+import java.util.Map;
+
 public class Bill {
+    private Customer customer;
+    private Map<Product, Integer> products;
+
+    /**
+     * Add a product with a quantity in the bill
+     * @param product product to add
+     * @param quantity quantity of the product to add
+     */
+    public void addProduct(Product product, Integer quantity){
+        this.products.put(product, quantity);
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public Map<Product, Integer> getProducts() {
+        return products;
+    }
 }
