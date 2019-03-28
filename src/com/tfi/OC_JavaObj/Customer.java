9f10a08a85f4ed7e953addd0fe6e2cf38005da4b
Tristan Figour <noblaya@gmail.com>
2019-03-28 12:56:37 +0100

    Customer Class :
    - Add Getter to fullName, adresse

--- src/com/tfi/OC_JavaObj/Customer.java
+++ src/com/tfi/OC_JavaObj/Customer.java
@@ -3,4 +3,11 @@ package com.tfi.OC_JavaObj;
 public class Customer {
     String fullName;
     String adress;
+
+    public String getFullName() {
+        return fullName;
+    }
+    public String getAdress() {
+        return adress;
+    }
 }
