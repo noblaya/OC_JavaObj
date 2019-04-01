09961d2ec7e42ad77e53a52211461cb6f11ca555
Tristan Figour <noblaya@gmail.com>
2019-04-01 19:55:21 +0200

    Make delivery System with :
    - Take Away delivery
    - Relay Delivery
    - Direct Delivery
    - Express Delivery

--- src/com/tfi/OC_JavaObj/shop/ExpressDelivery.java
+++ src/com/tfi/OC_JavaObj/shop/ExpressDelivery.java
@@ -1,4 +1,19 @@
 package com.tfi.OC_JavaObj.shop;
 
-public class ExpressDelivery {
+public class ExpressDelivery implements Delivery{
+    String city = " ";
+
+    public ExpressDelivery(String city) {
+        this.city = city;
+    }
+
+    @Override
+    public double getPrice() {
+        if (city.equals("Paris") || city.equals("paris")) {
+            return 6.99;
+        }
+        else {
+            return 9.99;
+        }
+    }
 }
