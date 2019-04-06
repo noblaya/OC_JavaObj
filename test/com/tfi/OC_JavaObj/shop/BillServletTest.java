e62de6fdab966f7bb0fd5c5c83d16e80c5ec1520
Tristan Figour <noblaya@gmail.com>
2019-04-06 15:56:22 +0200

    Solve Errors

--- test/com/tfi/OC_JavaObj/shop/BillServletTest.java
+++ test/com/tfi/OC_JavaObj/shop/BillServletTest.java
@@ -12,7 +12,7 @@ public class BillServletTest {
     @Test
     public void Given_twoParams_When_splitParans_Then_fillMapCorrectly() {
         BillServlet billServlet = new BillServlet();
-        Map<String, String> params = billServlet.splitParameters("p1=v1&p2=v2", keyAndValue);
+        Map<String, String> params = billServlet.splitParameters("p1=v1&p2=v2");
         assertEquals(2, params.size());
         assertEquals("v1", params.get("p1"));
         assertEquals("v2", params.get("p2"));
@@ -20,14 +20,14 @@ public class BillServletTest {
     @Test
     public void Given_OneEmptyParamsAndOneGoodParams_When_splitParans_Then_fillMapCorrectly() {
         BillServlet billServlet = new BillServlet();
-        Map<String, String> params = billServlet.splitParameters("p1=&p2=v2", keyAndValue);
+        Map<String, String> params = billServlet.splitParameters("p1=&p2=v2");
         assertEquals(1, params.size());
         assertEquals("v2", params.get("p2"));
     }
     @Test
     public void Given_NoParams_When_splitParans_Then_fillMapCorrectly() {
         BillServlet billServlet = new BillServlet();
-        Map<String, String> params = billServlet.splitParameters("test", keyAndValue);
+        Map<String, String> params = billServlet.splitParameters("test");
         assertEquals(0, params.size());
     }
 }
